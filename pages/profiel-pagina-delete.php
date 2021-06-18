35fee54192306e1b49d50c7328c6a76ece1e8eda
sito777 <68819756+sito777@users.noreply.github.com>
2021-06-18 14:04:23 +0200

    delete

--- pages/profiel-pagina-delete.php
+++ pages/profiel-pagina-delete.php
@@ -1,23 +1,43 @@
 <?php 
-require_once('./temp/header.php'); 
+require_once('./temp/header.php');
+session_start();
+$user = $_SESSION['username'];
+$stmt = $conn->prepare("SELECT role FROM user WHERE username LIKE :username");
+$stmt->bindParam(':username', $_SESSION['username']);
+$stmt->execute();
+
+$role = $stmt->fetchColumn();
+
+
+$stmt = $conn->prepare("SELECT user_id FROM user WHERE username LIKE :username");
+$stmt->bindParam(':username', $_SESSION['username']);
+$stmt->execute();
+
+$user_id = $stmt->fetchColumn();
+$_SESSION['user'] = $user_id ;
 ?>
 
-<!-- company  -->
 <?php
-$sql = "DELETE FROM comapny_id WHERE id = ?"
-$stmt = $conn->prepare($sql);
-$stmt->execute([$id])
+// company 
+if($role == 1){
+    $sql = "DELETE FROM comapny_id WHERE user_id LIKE user_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':user_id', $user_id);
+    $stmt->execute();
+    header('location: index.php?page=home');
+}
 ?>
 
-<!-- intern -->
-<?php
-$sql = "DELETE FROM inter_id WHERE id = ?"
-$stmt = $conn->prepare($sql);
-$stmt->execute([$id])
+<?php 
+// intern 
+if($role == 2){
+    $sql = "DELETE FROM itern WHERE user_id LIKE user_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':user_id', $user_id);
+    $stmt->execute();
+    header('location: index.php?page=home');
+}
 ?>
-
-
-
 
 <?php 
 require_once('./temp/footer.php');
